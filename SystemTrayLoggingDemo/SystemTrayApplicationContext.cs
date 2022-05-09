94d096def118c187fa5712a477d32549f8d581de
Nick Charlton <nick@nickcharlton.net>
2022-05-09 22:12:59 +0100

    Launch a system tray icon, with a single Exit item
    This uses the built-in "Application" icon from the Visual Studio Image
    Library, which avoids us needing to deal with any additional assets.
    
    This also removes the template `Form1`, as with this context it is no
    longer used.
    
    https://www.microsoft.com/en-gb/download/details.aspx?id=35825
    https://stackoverflow.com/a/10250051
    https://stackoverflow.com/a/71701184

---
+++ SystemTrayLoggingDemo/SystemTrayApplicationContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SystemTrayLoggingDemo
+{
+    public class SystemTrayApplicationContext : ApplicationContext
+    {
+        private readonly NotifyIcon _trayIcon;
+
+        public SystemTrayApplicationContext() 
+        {
+            _trayIcon = new NotifyIcon()
+            {
+                Icon = new Icon(SystemIcons.Application, 40, 40),
+                ContextMenuStrip = new ContextMenuStrip()
+                {
+                    Items = {new ToolStripMenuItem("Exit", null, Exit)}
+                },
+                Visible = true
+            };
+        }
+
+        private void Exit(object? sender, EventArgs e)
+        {
+            _trayIcon.Visible = false;
+            Application.Exit();
+        }
+    }
+}
